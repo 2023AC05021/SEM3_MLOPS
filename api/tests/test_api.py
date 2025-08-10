214f4bfea0627a3090f532bbfcf7d59f3d6e462c
asief hussain <2023AC05021@wilp.bits-pilani.ac.in>
2025-08-10 20:15:53 +0530

    check files

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -40,7 +40,8 @@ def test_successful_prediction():
         "Longitude": -122.23   # Block group longitude
     }
 
-    print([p.name for p in (Path.cwd() / "api" / "models" / "saved_models").glob('*') if p.is_file()])
+    obj=(Path.cwd() / "api" / "models" / "saved_models").glob('*')
+    print([p.name for p in obj if p.is_file() ])
 
     # Make a POST request to /predict with the valid payload
     response = client.post("/predict", json=valid_payload)
